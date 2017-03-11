2b21f76fc857666307d4818824f687713cb83ed7
SolitonNew <adm@soliton.org.ua>
2017-03-11 04:29:26 +0200

    1

--- server/lan_control/local_control/local_control_demon.py
+++ server/lan_control/local_control/local_control_demon.py
@@ -14,21 +14,23 @@ print(
 
 db = DBConnector()
 lastSpeechId = -1
-for rec in db.select("select MAX(ID) from app_control_speech"):
+for rec in db.select("select MAX(ID) from app_control_exe_queue"):
     lastSpeechId = rec[0]
 if lastSpeechId == None:
     lastSpeechId = -1
 
 while True:
-    for rec in db.select("select ID, SPEECH_AUDIO_ID, SPEECH_TYPE from app_control_speech "
+    for rec in db.select("select ID, EXE_TYPE, SPEECH_AUDIO_ID, SPEECH_TYPE "
+                         "  from app_control_exe_queue "
                          " where ID > %s "
                          "order by ID" % (lastSpeechId)):
         lastSpeechId = rec[0]
-        try:
-            print("Звучит: %s" % (rec[1]))
-            subprocess.call("aplay /home/pyhome/server/executor/%s.wav" % (str(rec[2], "utf-8")), shell=True)
-            f_name = "/var/tmp/wisehouse/audio_%s.wav" % (rec[1])
-            subprocess.call("aplay %s" % (f_name), shell=True)
-        except Exception as e:
-            print(e)
+        if str(rec[1], "utf-8") == "speech":
+            try:
+                print("Звучит: %s" % (rec[2]))
+                subprocess.call("aplay /home/pyhome/server/executor/%s.wav" % (str(rec[3], "utf-8")), shell=True)
+                f_name = "/var/tmp/wisehouse/audio_%s.wav" % (rec[2])
+                subprocess.call("aplay %s" % (f_name), shell=True)
+            except Exception as e:
+                print(e)
     time.sleep(0.1)

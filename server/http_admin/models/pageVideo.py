dc0febbf4ba158badb3028dc45b181d11614d18c
root <soliton_new@soliton.org.ua>
2017-08-03 20:29:50 +0300

    Video

--- server/http_admin/models/pageVideo.py
+++ server/http_admin/models/pageVideo.py
@@ -8,3 +8,9 @@ class PageVideo(BaseForm):
     def create_widgets(self):
         ls = List("CAMERA_LIST", "ID", "NAME", "select ID, NAME from plan_video order by NAME")
         self.add_widget(ls)
+
+    def query(self, query_type):
+        if query_type == "URL":
+            for rec in self.db.select("select URL from plan_video where ID = %s", [self.param_str('key')]):
+                return str(rec[0], "utf-8")
+        return ""

0b45d65c60aaf3cd25567e777f9684a635e46176
Ubuntu <adm@soliton.org.ua>
2020-09-29 17:10:26 +0300

    Step two

---
+++ server/lan_control/desktop-control/src/index.js
@@ -0,0 +1,35 @@
+import { app, BrowserWindow } from 'electron';
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 300,
+    height: 800,
+    autoHideMenuBar: true,
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});

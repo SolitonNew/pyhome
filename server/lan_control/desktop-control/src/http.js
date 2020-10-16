d051e3d00d5508ff16541b4c7b3947a0f449d4a3
Ubuntu <adm@soliton.org.ua>
2020-10-16 11:24:25 +0300

    Check access for http media server

--- server/lan_control/desktop-control/src/http.js
+++ server/lan_control/desktop-control/src/http.js
@@ -3,9 +3,26 @@ const http = require('http');
 function initHTTP() {
     http.createServer(function (request, response) {
         let fs = require('fs');
-        fs.createReadStream(request.url).pipe(response);
+        let file = request.url;
+        let id = -1;
+        try {
+            id = parseInt(file);
+        } catch(e) {}
+        
+        if (id > 0) { // Секция для отдачи моих медиаданных
+            
+        } else {
+            let ext = file.substring(file.length - 4).toUpperCase();
+            let ext_ok = (['.MP3', '.WAV'].indexOf(ext) > -1);
+            let appData = app.getPath('appData');
+            let path_ok = (file.indexOf('./') == -1) && 
+                          (file.indexOf('~/') == -1) && 
+                          (file.substring(0, appData.length) == appData);
+            if (ext_ok && path_ok) {
+                fs.createReadStream(request.url).pipe(response);                
+            } else {
+                response.writeHead(404);
+            }
+        }
     }).listen(8092);
-    
-    
-    //document.getElementById('audioSpeech').src = 'http://127.0.0.1:8092/home/soliton/.config/Electron/audio/n.wav';
 }

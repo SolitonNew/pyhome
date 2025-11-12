323c43c52d54231ae49028b8864ab724416ecee3
Alexandr Moklyak <SolitonNew@gmail.com>
2025-11-12 11:30:50 +0200

    system init

--- server/utils/init_system.py
+++ server/utils/init_system.py
@@ -10,6 +10,6 @@ for rec in db.select("select ID, VARIABLE_ID, CHANGE_DATE, VALUE, FROM_ID "
     db.IUD("insert into core_variable_changes_mem "
            " (ID, VARIABLE_ID, CHANGE_DATE, VALUE, FROM_ID) "
            " values "
-           " (%s, %s, '%s', %s, '%s')", (rec[0], rec[1], rec[2], rec[3], rec[4]))
+           " (%s, %s, %s, %s, '%s')", (rec[0], rec[1], rec[2], rec[3], rec[4]))
     
 print("INIT SYSTEM FINISHED")

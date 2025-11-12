095bb739757181053ec5028a4a8a6b6c01b8a3fe
Alexandr Moklyak <SolitonNew@gmail.com>
2025-11-12 11:27:17 +0200

    system init

--- server/utils/init_system.py
+++ server/utils/init_system.py
@@ -4,12 +4,12 @@ db = DBConnector()
 
 db.IUD("delete from core_variable_changes_mem");
 
-for rec in db.select("select VARIABLE_ID, CHANGE_DATE, VALUE, FROM_ID "
+for rec in db.select("select ID, VARIABLE_ID, CHANGE_DATE, VALUE, FROM_ID "
                      "  from core_variable_changes "
                      " where CHANGE_DATE >= current_timestamp() - interval 3 hour"):
     db.IUD("insert into core_variable_changes_mem "
-           " (VARIABLE_ID, CHANGE_DATE, VALUE, FROM_ID) "
+           " (ID, VARIABLE_ID, CHANGE_DATE, VALUE, FROM_ID) "
            " values "
-           " (%s, '%s', %s, '%s')", (rec[0], rec[1], rec[2], rec[3]))
+           " (%s, %s, '%s', %s, '%s')", (rec[0], rec[1], rec[2], rec[3], rec[4]))
     
 print("INIT SYSTEM FINISHED")
